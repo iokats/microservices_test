1d7810a2442d114edbe86ec5c89bc8360dc4e221
katsatos <katsatosjohn@gmail.com>
2024-03-16 09:24:50 +0200

    Extended the API of the microservices to support create/ delete operations

--- api/src/main/kotlin/se/magnus/api/core/review/ReviewService.kt
+++ api/src/main/kotlin/se/magnus/api/core/review/ReviewService.kt
@@ -1,16 +1,40 @@
 package se.magnus.api.core.review
 
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
 
 interface ReviewService {
 
     /**
+     * Sample usage, see below.
+     *
+     * curl -X POST $HOST:$PORT/review \
+     *   -H "Content-Type: application/json" --data \
+     *   '{"productId":123,"reviewId":456,"author":"me","subject":"yada, yada, yada","content":"yada, yada, yada"}'
+     *
+     * @param body A JSON representation of the new review
+     * @return A JSON representation of the newly created review
+     */
+    @PostMapping(value = ["/review"], consumes = ["application/json"], produces = ["application/json"])
+    fun createReview(@RequestBody body: Review): Review
+
+    /**
      * Sample usage: "curl $HOST:$PORT/review?productId=1".
      *
-     * @param productId Id of the product
+     * @param productId ID of the product
      * @return the reviews of the product
      */
     @GetMapping(value = ["/review"], produces = ["application/json"])
     fun getReviews(@RequestParam(value = "productId", required = true) productId: Int): List<Review>
+
+    /**
+     * Sample usage: "curl -X DELETE $HOST:$PORT/review?productId=1".
+     *
+     * @param productId ID of the product
+     */
+    @DeleteMapping(value = ["/review"])
+    fun deleteReview(@RequestParam(value = "productId", required = true) productId: Int)
 }
